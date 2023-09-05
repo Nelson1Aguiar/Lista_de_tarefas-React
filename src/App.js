7116731f091b4e265eb84034443985398e9cf0e1
Your Nelson Sales <nelsonsales20.ns@gmail.com>
2023-09-04 21:14:24 -0300

    Funcionalidades de add e botao de remove nao implementado com função

--- src/App.js
+++ src/App.js
@@ -1,37 +1,61 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { MdDelete } from 'react-icons/md';
 
 function App() {
-    const enter=13
-    const esc=27
-    const [value,setValue]=useState('')
-    const onChange=(event)=>{
+    const enter = 13;
+    const esc = 27;
+    const [value, setValue] = useState('');
+    const [lista, setLista] = useState([]);
+
+    const incluir = () => {
+        setLista([
+            ...lista,
+            { id: new Date().getTime(), title: value, checked: false },
+        ]);
+    };
+    const onChange = (event) => {
         setValue(event.target.value);
-    }
-    const apagar=()=>{
-        setValue("")
-    }
-    const onKeyDown=(event)=>{
-        if(event.which===enter){
-            console.log(value)
+    };
+    const apagar = () => {
+        setValue('');
+    };
+    const onKeyDown = (event) => {
+        if (event.which === enter) {
+            // eslint-disable-next-line no-console
+            incluir();
+        } else if (event.which === esc) {
+            apagar();
         }
-        else if(event.which===esc){
-            apagar()
-        }
-    }
+    };
 
-
-  return(
-  <section id='app' className='container'>
-    <header>
-        <h1 className='title'>todo</h1>
-    </header>
-    <section className='main'>
-        <input className='new-todo' placeholder='Digite sua tarefa...'
-         value={value}onChange={onChange} onKeyDown={onKeyDown}/>
-    </section>
-  </section>
-  )
+    return (
+        <section id="app" className="container">
+            <header>
+                <h1 className="title">todo</h1>
+            </header>
+            <section className="main">
+                <input
+                    className="new-todo"
+                    placeholder="Digite sua tarefa..."
+                    value={value}
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}
+                />
+                <ul className="todo-list">
+                    {lista.map((paraFazer) => (
+                        <li key={paraFazer.id.toString()}>
+                            <span className="todo">{paraFazer.title}</span>
+                            <button className="remove" type="button">
+                                <MdDelete size={28} />
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </section>
+    );
 }
 
 export default App;
